344b406dcca92a1a757b77d6f0812cbba2d5d87c
Taiki HISAMURA <taiki.hisamura@crowdworks.co.jp>
2019-09-04 22:08:10 +0900

    ログを出力できるように改修

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/gorilla/websocket"
+)
 
 type client struct {
 	socket *websocket.Conn
@@ -11,6 +13,7 @@ type client struct {
 func (c *client) read()  {
 	for {
 		if _, msg, err := c.socket.ReadMessage(); err == nil {
+
 		  c.room.forward <- msg
 		} else {
 			break
@@ -22,6 +25,7 @@ func (c *client) read()  {
 func (c *client) write() {
 	for msg := range c.send {
 		if err := c.socket.WriteMessage(websocket.TextMessage, msg);
+
 		  err != nil {
 		  	break
 		}
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"path/filepath"
-	"text/template"
+	"flag"
 	"log"
 	"net/http"
+	"path/filepath"
 	"sync"
+	"text/template"
 )
 
 type templateHandler struct {
@@ -20,15 +21,18 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
 		template.Must(template.ParseFiles(filepath.Join("templates",
 			t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
 	go r.run()
+	log.Println("webサーバーを起動します。ポート: ", *addr)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
